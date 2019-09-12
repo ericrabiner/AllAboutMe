a03b9b5210e2db663d8a8c81e64a73450eea7199
Eric Rabiner <erabiner@myseneca.ca>
2019-09-12 15:41:19 -0700

    First

---
+++ AllAboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllAboutMe
+//
+//  Created by Eric rabiner on 2019-09-12.
+//  Copyright © 2019 Eric Rabiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
